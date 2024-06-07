1f80bea1819fa99c430365bd7df659d1be99942e
Michele Villani <michele123villani@gmail.com>
2024-06-07 14:51:09 +0200

    Code cleaning and small fixes

--- assets/js/map-final.js
+++ assets/js/map-final.js
@@ -226,8 +226,6 @@ susMap_rec.setVisible(false);
 merged.setVisible(false);
 outline.setVisible(false);
 
-
-
 // Map Initialization
 let map = new Map({
     target: document.getElementById('map'),
@@ -251,21 +249,16 @@ map.addControl(
 );
 map.addControl(new ZoomToExtent({
     extent: [
-        // 813079.7791264898,
-        // 5929220.284081122,
-        // 848966.9639063801,
-        // 5936863.986909639, //TODO 
-        1108000 - 9000,
-        5745000 - 9000,
-        1108000 + 9000,
-        5745000 + 9000,
+        1106000 - 14000,
+        5739000 - 14000,
+        1106000 + 14000,
+        5739000 + 14000,
     ],
   }),);
 
 var layerSwitcher = new LayerSwitcher({});
 map.addControl(layerSwitcher);
 
-//OPTIONAL
 //Add the Bing Maps layers
 var BING_MAPS_KEY = "AqbDxABFot3cmpxfshRqLmg8UTuPv_bg69Ej3d5AkGmjaJy_w5eFSSbOzoHeN2_H";
 var bingRoads = new Tile({
@@ -287,125 +280,6 @@ var bingAerial = new Tile({
     })
 });
 basemapLayers.getLayers().extend([bingRoads, bingAerial]);
-
-//Add the Stadia Maps layers
-// var stadiaWatercolor = new Tile({
-//     title: "Stadia Watercolor",
-//     type: "base",
-//     visible: false,
-//     source: new StadiaMaps({
-//         layer: 'stamen_watercolor'
-//     })
-// })
-// var stadiaToner = new Tile({
-//     title: "Stadia Toner",
-//     type: "base",
-//     visible: false,
-//     source: new StadiaMaps({
-//         layer: 'stamen_toner'
-//     })
-// })
-// basemapLayers.getLayers().extend([stadiaWatercolor, stadiaToner]);
-
-//Add the WFS layer
-// let vectorSource = new VectorSource({});
-// const vectorLayer = new Vector({
-//     title: "Colombia water areas",
-//     source: vectorSource,
-//     style: new Style({
-//         stroke: new Stroke({
-//             color: 'rgb(255, 102, 0)',
-//             width: 4
-//         })
-//     }),
-//     zIndex: 10
-// });
-// overlayLayers.getLayers().extend([vectorLayer]);
-
-
-// // This allows to use the function in a callback!
-// function loadFeatures(response) {
-//     vectorSource.addFeatures(new GeoJSON().readFeatures(response))
-// }
-// // This is not a good practice, but works for the jsonp.
-// window.loadFeatures = loadFeatures;
-
-// var base_url = "https://www.gis-geoserver.polimi.it/geoserver/gis/ows?";
-// var wfs_url = base_url;
-// wfs_url += "service=WFS&"
-// wfs_url += "version=2.0.0&"
-// wfs_url += "request=GetFeature&"
-// wfs_url += "typeName=gis%3ACOL_water_areas&"
-// wfs_url += "outputFormat=text%2Fjavascript&"
-// wfs_url += "srsname=EPSG:3857&"
-// wfs_url += "format_options=callback:loadFeatures"
-
-// // This will request the WFS layer once the map is rendered.
-// // Uses the map event 'postrender': https://openlayers.org/en/v8.2.0/apidoc/module-ol_MapEvent-MapEvent.html#event:postrender
-// map.once('postrender', (event) => {
-//     // Load the WFS layer
-//     $.ajax({ url: wfs_url, dataType: 'jsonp' });
-// })
-
-// //Add the code for the Pop-up
-// var container = document.getElementById('popup');
-// var content = document.getElementById('popup-content');
-// var closer = document.getElementById('popup-closer');
-
-// var popup = new Overlay({
-//     element: container
-// });
-// map.addOverlay(popup);
-
-// // This ensures that JQuery ($) is already available in the page.
-// $(document).ready(function () {
-//     map.on('singleclick', function (event) {
-//         //This iterates over all the features that are located on the pixel of the click (can be many)
-//         var feature = map.forEachFeatureAtPixel(event.pixel, function (feature, layer) {
-//             return feature;
-//         });
-
-//         //If there is a feature, open the popup by setting a position to it and put the data from the feature
-//         if (feature != null) {
-//             var pixel = event.pixel;
-//             var coord = map.getCoordinateFromPixel(pixel);
-//             popup.setPosition(coord);
-//             content.innerHTML =
-//                 '<h5>Colombia Water Areas</h5><br><b>Name: </b>' +
-//                 feature.get('NAME') +
-//                 '</br><b>Description: </b>' +
-//                 feature.get('HYC_DESCRI');
-//         } else {
-//             //Only if the colombiaRoads layer is visible, do the GetFeatureInfo request
-//             if (colombiaRoads.getVisible()) {
-//                 var viewResolution = (map.getView().getResolution());
-//                 var url = colombiaRoads.getSource().getFeatureInfoUrl(event.coordinate, viewResolution, 'EPSG:3857', { 'INFO_FORMAT': 'text/html' });
-
-//                 if (url) {
-//                     var pixel = event.pixel;
-//                     var coord = map.getCoordinateFromPixel(pixel);
-//                     popup.setPosition(coord);
-//                     //We do again the AJAX request to get the data from the GetFeatureInfo request
-//                     $.ajax({ url: url })
-//                         .done((data) => {
-//                             //Put the data of the GetFeatureInfo response inside the pop-up
-//                             //The data that arrives is in HTML
-//                             content.innerHTML = data;
-//                         });
-//                 }
-//             }
-//         }
-//     });
-// });
-
-
-// // The click event handler for closing the popup.
-// closer.onclick = function () {
-//     popup.setPosition(undefined);
-//     closer.blur();
-//     return false;
-// };
-
 
 // Adding map event for pointermove
 map.on('pointermove', function (event) {
